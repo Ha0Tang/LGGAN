b6481e0f1b77f5430a1998847eee518926e4ee5a
Ha0Tang <haotang@pku.edu.cn>
2020-01-04 21:57:54 +0000

    Update dataset download script

---
+++ datasets/download_lggan_dataset.sh
@@ -0,0 +1,16 @@
+FILE=$1
+
+if [[ $FILE != "dayton" && $FILE != "cvusa"]]; 
+	then echo "Available datasets are dayton, cvusa"
+	exit 1
+fi
+
+echo "Specified [$FILE]"
+
+URL=http://disi.unitn.it/~hao.tang/uploads/datasets/LGGAN/$FILE.tar.gz
+TAR_FILE=./datasets/$FILE.tar.gz
+TARGET_DIR=./datasets/$FILE/
+wget -N $URL -O $TAR_FILE
+mkdir -p $TARGET_DIR
+tar -zxvf $TAR_FILE -C ./datasets/
+rm $TAR_FILE
