3793992d80bcf2e0485100591ebfed8b4ff01dff
Ha0Tang <haotang@pku.edu.cn>
2020-01-04 22:13:02 +0000

    Update readme

--- datasets/download_lggan_dataset.sh
+++ datasets/download_lggan_dataset.sh
@@ -1,13 +1,14 @@
 FILE=$1
 
-if [[ $FILE != "dayton" && $FILE != "cvusa"]]; 
+if [[ $FILE != "dayton" && $FILE != "cvusa" ]]; 
 	then echo "Available datasets are dayton, cvusa"
 	exit 1
 fi
 
+
 echo "Specified [$FILE]"
 
-URL=http://disi.unitn.it/~hao.tang/uploads/datasets/LGGAN/$FILE.tar.gz
+URL=http://disi.unitn.it/~hao.tang/uploads/datasets/SelectionGAN/$FILE.tar.gz
 TAR_FILE=./datasets/$FILE.tar.gz
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $TAR_FILE
